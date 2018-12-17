ea8519df4aa2633805ca2ec72519e84eb1b0947d
Emerson Pourghaed <Emerson.Pourghaed@txdmv.gov>
2018-12-17 13:30:41 -0600

    feat: index.php set up initial pagination

--- include/blog-post-template.php
+++ include/blog-post-template.php
@@ -1,12 +1,42 @@
 <div class="row" style="padding: 0 10%;">
 
 <?php
-
 // since the file is an include, the JSON file is being accessed from outside /include
 $json = json_decode(file_get_contents('./assets/data/index.json'), true);
 $jsonArr = $json['arr'];
+$jsonCount = count($jsonArr);
+$val = 5;
 
-for ($i = 0; $i < 3; $i++) {
+if (isset($_GET['pagination']) && $_GET['pagination'] != 1) {
+  $val *= $_GET['pagination'];
+
+  $headlineStart = $val - 5;
+  $headlineCount = $val - 2;
+  $jsonStart = $val - 1;
+  $jsonEnd = $val;
+
+  $arr = [$headlineStart, $headlineCount, $jsonStart, $jsonEnd];
+
+  for ($d = 0; $d < count($arr); $d++) {
+    if ($arr[$d] >= $jsonCount) {
+      $arr[$d] = $jsonCount;
+    }
+    if($d == 3) {
+      $headlineStart = $arr[0];
+      $headlineCount = $arr[1];
+      $jsonStart = $arr[2];
+      $jsonEnd = $arr[3];
+    }
+  }
+
+} else {
+  $headlineStart = $val - 5;
+  $headlineCount = $val - 2;
+  $jsonStart = $val - 1;
+  $jsonEnd = $val;
+}
+
+for ($i = $headlineStart; $i < $headlineCount; $i++) {
   echo '
   <div data-aos-delay="0" class="col-sm bounce animated">
     <div class="card" style="width: 22rem;">
@@ -31,7 +61,7 @@ for ($i = 0; $i < 3; $i++) {
 
             <?php
 
-            for ($i = 3; $i < count($jsonArr); $i++) {
+            for ($jsonStart = 3; $i < $jsonEnd; $i++) {
               echo '
                 <div class="post-preview">
                     <a href="#">
@@ -44,7 +74,21 @@ for ($i = 0; $i < 3; $i++) {
             };
 
             ?>
-            <div class="clearfix"><button class="btn btn-primary float-right" type="button" style="background-color: rgb(0,0,0);">Older Posts&nbsp;⇒</button></div>
+
+
+            <?php
+            if (isset($_GET['pagination']) && $_GET['pagination'] != 1) {
+              if($_GET['pagination']) {
+                echo '<a href="./index.php?pagination=' . ($_GET['pagination'] - 1) . '"><div class="clearfix"><button class="btn btn-primary float-right" type="button" style="background-color: rgb(0,0,0);">Recent Posts&nbsp;⇒</button></div></a>';
+                if ($jsonEnd != $jsonCount) {
+                  echo '<a href="./index.php?pagination=' . ($_GET['pagination'] + 1) . '"><div class="clearfix"><button class="btn btn-primary float-right" type="button" style="background-color: rgb(0,0,0);">Older Posts&nbsp;⇒</button></div></a>';
+                }
+              }
+            } else {
+              echo '<a href="./index.php?pagination=2"><div class="clearfix"><button class="btn btn-primary float-right" type="button" style="background-color: rgb(0,0,0);">Older Posts&nbsp;⇒</button></div></a>';
+            }
+
+            ?>
         </div>
     </div>
 </div>
