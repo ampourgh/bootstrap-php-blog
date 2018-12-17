62a93073995daa1694dc836c9c395006e1775999
Emerson Pourghaed <Emerson.Pourghaed@txdmv.gov>
2018-12-17 14:03:07 -0600

    bug: fix server request uri

--- include/nav-links.php
+++ include/nav-links.php
@@ -2,15 +2,17 @@
   $folder = '/Bootstrap-Studio-Design/';
   $arr = ['index.php', 'about.php', 'contact.php', 'post.php'];
   $arr2 = ['home', 'table', 'image', 'calendar-times'];
+  $withOutParams = explode('?', $_SERVER['REQUEST_URI'], 2);
 
   for ($i = 0; $i < count($arr); $i++) {
     $fontAwesomeIMG = '<li class="nav-item';
-    $fontAwesomeIMG .= (($folder . $arr[$i]) === $_SERVER['REQUEST_URI']) ? ' active' : '';
+    $fontAwesomeIMG .= (($folder . $arr[$i]) === $withOutParams[0]) ? ' active' : '';
     $fontAwesomeIMG .= '"><a class="nav-link" href="' . $folder . $arr[$i] . '"><i class="fa';
     $fontAwesomeIMG .= ($arr2 == 'home') ? 's' : '';
     $fontAwesomeIMG .= ' fa-' . $arr2[$i] . '"></i> ';
-    $fontAwesomeIMG .= (($folder . $arr[$i]) === $_SERVER['REQUEST_URI']) ? '<span class="sr-only">(current)</span>' : '';
+    $fontAwesomeIMG .= (($folder . $arr[$i]) === $withOutParams[0]) ? '<span class="sr-only">(current)</span>' : '';
     $fontAwesomeIMG .= '</a></li>';
     echo $fontAwesomeIMG;
   }
+
 ?>
